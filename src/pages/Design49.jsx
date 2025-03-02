0391ffc02d47eaadcdd42bf8ff10fab43b68fb04
git12nazneen <lipinazneen65@gmail.com>
2025-03-03 00:29:45 +0600

    design 49 done

--- src/pages/Design49.jsx
+++ src/pages/Design49.jsx
@@ -23,6 +23,7 @@ const Design49 = () => {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 text-black">
           <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
         </svg>
+  
       </div>
 
       {/* Main content section */}
@@ -42,19 +43,24 @@ const Design49 = () => {
         </div>
 
         {/* Right side image and text */}
-        <div className="relative flex justify-center">
-          <img src={picture} alt="Person with flower crown" className="rounded-lg" />
-          <div className="absolute bottom-16 right-0 transform rotate-12 bg-yellow-100 p-2 rounded shadow-sm text-xs max-w-[180px]">
-            <p className="text-black font-light">
-              Be yourself. Let who you are, what you are, speak for itself.
-            </p>
-          </div>
-          <div className="absolute bottom-32 left-0 transform -rotate-12 bg-yellow-100 p-2 rounded shadow-sm text-xs max-w-[180px]">
-            <p className="text-black font-light">
-              Beauty is about enhancing what you have.
-            </p>
-          </div>
-        </div>
+        <div className=" flex justify-center">
+  <img src={picture} alt="Person with flower crown" className="rounded-lg" />
+
+  {/* First box with rotated text */}
+  {/* <div className="absolute bottom-16 right-0 transform rotate-12 bg-yellow-100 p-4 rounded shadow-sm text-xs max-w-[180px]">
+    <p className="text-black font-light">
+      Be yourself. Let who you are, what you are, speak for itself.
+    </p>
+  </div> */}
+
+  {/* Second box with rotated text */}
+  {/* <div className="absolute bottom-32 left-0 transform -rotate-12 bg-yellow-100 p-4 rounded shadow-sm text-xs max-w-[180px]">
+    <p className="text-black font-light">
+      Beauty is about enhancing what you have.
+    </p>
+  </div> */}
+</div>
+
       </div>
     </div>
   );
