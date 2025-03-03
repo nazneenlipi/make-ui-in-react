276aa95ae7c1c1ec6e7428211bb36151e0f967d3
git12nazneen <lipinazneen65@gmail.com>
2025-03-03 22:36:14 +0600

    done design 50

--- src/pages/Design50.jsx
+++ src/pages/Design50.jsx
@@ -10,13 +10,11 @@ import sic from "../assets/banner/Rectangle 86.jpg";
 const Design50 = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center w-full  bg-cover bg-center bg-no-repeat"
-      style={{
-        backgroundImage: `url(${backgroundImg})`,
-      }}
+      className="flex flex-col items-center justify-center w-full bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${backgroundImg})` }}
     >
       <div className="w-full px-6 py-12 md:py-16 lg:py-20 max-w-6xl mx-auto text-center">
-        <h1 className="text-3xl md:text-5xl font-bold text-black leading-relaxed ">
+        <h1 className="text-3xl md:text-5xl font-bold text-black leading-relaxed">
           We deliver beautiful and <br /> reliable designs you need
         </h1>
         <p className="mt-4 text-gray-600 text-lg max-w-2xl mx-auto">
@@ -24,32 +22,48 @@ const Design50 = () => {
           imperdiet sed id elementum. Quam vel aliquam sit vulputate. Faucibus
           nec gravida ipsum pulvinar vel.
         </p>
-        <button className="mt-6 mb-6 px-6 py-3 bg-black text-white text-lg rounded-lg hover:bg-gray-900">
+        <button className="mt-6 mb-6 px-6 py-3 bg-black text-black text-lg rounded-lg hover:bg-gray-900">
           Get started
         </button>
 
         {/* Grid Section */}
         <div className="mt-12 flex gap-6">
           {/* First div - 40% width */}
-          <div className="relative  overflow-hidden  w-[40%] flex flex-col  gap-5">
+          <div className="relative overflow-hidden w-[40%] flex flex-col gap-5">
             <div className="flex flex-row gap-5">
-              <img
-                src={two}
-                alt="Title 1"
-                className="flex-1 w-full h-[190px] rounded-3xl object-cover"
-              />
-              <img
-                src={three}
-                alt="Title 1"
-                className="flex-1 w-full h-[190px] rounded-3xl object-cover"
-              />
+              {/* Image 1 */}
+              <div className="relative flex-1">
+                <img
+                  src={two}
+                  alt="Title 1"
+                  className="w-full h-[190px] rounded-3xl object-cover"
+                />
+                <div className="absolute inset-0  rounded-3xl"></div>
+                <p className="absolute bottom-0 left-10 w-32 flex items-center justify-center text-black text-sm">
+                  Lorem, ipsum dolor sit amet consectetur 
+                </p>
+              </div>
+              {/* Image 2 */}
+              <div className="relative flex-1">
+                <img
+                  src={three}
+                  alt="Title 2"
+                  className="w-full h-[190px] rounded-3xl object-cover"
+                />
+                <div className="absolute inset-0  rounded-3xl"></div>
+                <p className="absolute bottom-0 left-10 w-32 flex items-center justify-center text-black text-sm">
+                  Lorem, ipsum dolor sit amet consectetur 
+                </p>
+              </div>
             </div>
-            <div>
+            {/* Image 3 */}
+            <div className="relative">
               <img
                 src={one}
-                alt="Title 1"
+                alt="Title 3"
                 className="w-full h-[190px] rounded-3xl object-cover"
               />
+             
             </div>
           </div>
 
@@ -57,29 +71,40 @@ const Design50 = () => {
           <div className="relative w-[40%]">
             <img
               src={five}
-              alt="Title 2"
+              alt="Title 4"
               className="w-full h-[400px] rounded-3xl object-cover"
             />
+            <div className="absolute inset-0  rounded-3xl"></div>
+            <p className="absolute top-3 right-4 flex items-center justify-center text-black font-semibold text-2xl">
+             Titile 1
+            </p>
           </div>
 
           {/* Third div - 20% width */}
-          <div className="relative  flex flex-col gap-5">
-            {/* First image block */}
+          <div className="relative flex flex-col gap-5">
+            {/* Image 5 */}
             <div className="relative">
               <img
                 src={froun}
-                alt="Title 3"
-                className="w-full rounded-3xl h-[190px] object-cover"
+                alt="Title 5"
+                className="w-full h-[190px] rounded-3xl object-cover"
               />
+              <div className="absolute inset-0  rounded-3xl"></div>
+              <p className="absolute bottom-0 left-10 w-32 flex items-center justify-center text-black text-sm">
+                  Lorem, ipsum dolor sit amet consectetur 
+                </p>
             </div>
-
-            {/* Second image block */}
+            {/* Image 6 */}
             <div className="relative">
               <img
                 src={sic}
-                alt="Title 3"
+                alt="Title 6"
                 className="w-full h-[190px] rounded-3xl object-cover"
               />
+              <div className="absolute inset-0  rounded-3xl"></div>
+              <p className="absolute bottom-0 left-10 w-32 flex items-center justify-center text-black text-sm">
+                  Lorem, ipsum dolor sit amet consectetur 
+                </p>
             </div>
           </div>
         </div>
