dc293e477e00a59509869011c76758e0aa852210
git12nazneen <lipinazneen65@gmail.com>
2025-03-04 01:27:40 +0600

    responsive done

--- src/pages/Design48.jsx
+++ src/pages/Design48.jsx
@@ -5,6 +5,7 @@ import circle from "../assets/banner/font Circle.png";
 import { BsTwitter } from "react-icons/bs";
 import { Facebook, Youtube } from "lucide-react";
 import { PiInstagramLogo } from "react-icons/pi";
+
 const Design48 = () => {
   return (
     <div className="bg-[#fbd745] text-black flex flex-col items-center justify-center overflow-hidden relative">
@@ -20,11 +21,11 @@ const Design48 = () => {
           </h1>
           <a
             href="#_"
-            class="relative inline-block px-6 py-3 font-medium group"
+            className="relative inline-block px-6 py-3 font-medium group"
           >
-            <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black rounded-full group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-            <span class="absolute inset-0 w-full h-full bg-[#fbd745] border-2 border-black rounded-full group-hover:bg-black"></span>
-            <span class="relative text-black group-hover:text-white text-4xl md:text-5xl lg:text-4xl font-bold">
+            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black rounded-full group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+            <span className="absolute inset-0 w-full h-full bg-[#fbd745] border-2 border-black rounded-full group-hover:bg-black"></span>
+            <span className="relative text-black group-hover:text-white text-4xl md:text-5xl lg:text-4xl font-bold">
               digital
             </span>
           </a>
@@ -59,28 +60,32 @@ const Design48 = () => {
       {/* Background Image with Overlaying Button and Stats */}
       <div className="relative w-full">
         <img src={laptop} alt="Laptop" className="w-screen h-96 object-cover" />
+        
+        {/* Get Started Button and Stats Section */}
+        <div className="absolute top-2 left-0 md:left-10 lg:left-44  w-full flex flex-col md:flex-row items-center justify-center gap-10">
+          {/* Get Started Button */}
+          <button className="bg-black text-white px-6 py-3 rounded-full font-semibold z-10">
+            Get started
+          </button>
 
-        {/* Get Started Button */}
-        <button className="absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black text-white px-6 py-3 rounded-full font-semibold z-10">
-          Get started
-        </button>
-
-        {/* Stats Section */}
-        <div className="absolute top-3 right-10 transform -translate-x-1/2 flex items-center space-x-6 z-10 px-8 py-4 bg-transparent gap-10 rounded-full  before:absolute before:inset-0 before:border-2 before:border-black before:rounded-full before:-translate-x-1 before:-translate-y-1 after:absolute after:inset-0 after:border-2 after:border-black after:rounded-full after:translate-x-1 after:translate-y-1 w-80 h-16">
-  {["113+", "274K", "179+"].map((stat, index) => (
-    <div key={index} className="text-lg font-bold text-black">
-      {stat}
-    </div>
-  ))}
-</div>
-
+          {/* Stats Section */}
+          <div className="relative flex items-center space-x-6 z-10 px-8 py-4 bg-transparent gap-10 rounded-full  
+            before:absolute before:inset-0 before:border-2 before:border-black before:rounded-full before:-translate-x-1 before:-translate-y-1  
+            after:absolute after:inset-0 after:border-2 after:border-black after:rounded-full after:translate-x-1 after:translate-y-1 w-80 h-16">
+            {["113+", "274K", "179+"].map((stat, index) => (
+              <div key={index} className="text-lg font-bold text-black">
+                {stat}
+              </div>
+            ))}
+          </div>
+        </div>
 
         <img
           src={circle}
-          alt=""
+          alt="Decor Circle"
           className="absolute bottom-10 right-72 transform -translate-x-1/2 -translate-y-1/2  rounded-full z-10"
         />
-        <div className="flex gap-3 absolute bottom-6 right-44 transform -translate-x-1/2 -translate-y-1/2  rounded-full z-10">
+        <div className="flex gap-3 absolute bottom-6 right-44 transform -translate-x-1/2 -translate-y-1/2 rounded-full z-10">
           <BsTwitter className="size-8 bg-white rounded-full m-2 p-2" />
           <Facebook className="size-8 bg-white rounded-full m-2 p-2" />
           <Youtube className="size-8 bg-white rounded-full m-2 p-2" />
