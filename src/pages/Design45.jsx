b83317043fee622d5a0cb9b6bff25625e15b0a09
git12nazneen <lipinazneen65@gmail.com>
2025-03-05 02:49:25 +0600

    grid system

--- src/pages/Design45.jsx
+++ src/pages/Design45.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import img1 from "../assets/banner/Image left.jpg";
-import img2 from "../assets/banner/Image right.jpg";
-import img3 from "../assets/banner/Image bottom left - left.jpg";
-import img4 from "../assets/banner/Image bottom left - right.jpg";
+import img1 from "../assets/banner/a.jpeg";
+import img2 from "../assets/banner/b.jpeg";
+import img3 from "../assets/banner/c.jpeg";
+import img4 from "../assets/banner/d.jpeg";
 
 const Design45 = () => {
   return (
+    <section className="bg-[#33333380]">
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 max-w-7xl mx-auto p-6">
       <div className="flex flex-col">
         <div>
             <img src={img1} alt="" />
         </div>
         <div className="flex flex-row">
-            <img src={img3} alt="" className="w-[308px]" />
-            <img src={img4} alt="" className="w-[308px]"/>
+            <img src={img3} alt="" className="w-[308px] h-60" />
+            <img src={img4} alt="" className="w-[308px] h-60"/>
         </div>
 
       </div>
       <div>
-        <img src={img2} alt="" className="md:h-[620px] lg:h-auto h-auto" />
+        <img src={img2} alt="" className="md:h-[620px] lg:h-[650px] h-auto w-full" />
       </div>
     </div>
+    </section>
   );
 };
 
