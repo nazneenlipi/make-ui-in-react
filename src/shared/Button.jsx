6aabb2bdd0854f28b2f3d79359d88e839e18eed9
git12nazneen <lipinazneen65@gmail.com>
2025-04-21 14:18:26 +0600

    style

--- src/shared/Button.jsx
+++ src/shared/Button.jsx
@@ -1,6 +1,4 @@
-// Button.js
 import React from 'react';
-
 const Button = ({ text }) => {
     return (
         <button className="group relative bg-[#1a1a1a] text-white px-6 py-2 rounded-lg flex items-center space-x-2 hover:bg-black transition-colors">
