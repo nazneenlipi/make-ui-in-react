85b9ed3aefb49d17478a6c95913a64f877c31d2a
git12nazneen <lipinazneen65@gmail.com>
2025-04-23 22:08:07 +0600

    stle

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,6 @@ const Main = () => {
     script1.async = true;
     script1.src = `{https://www.googletagmanager.com/gtag/js?id=${import.meta.env.GOOGLE_ANALYTICS}}`;
     document.head.appendChild(script1);
-
     const script2 = document.createElement("script");
     script2.innerHTML = `
       window.dataLayer = window.dataLayer || [];
@@ -30,4 +29,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Main />
   </React.StrictMode>,
 );
-
